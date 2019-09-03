ca1fe3826456b955f01cf5870af5a387304c8035
e7a894 <e7a894@outlook.jp>
2019-09-03 13:53:38 +0900

    chore: set sourcemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 import { Configuration } from '@nuxt/types';
 
 const config: Configuration = {
+  build: {
+    extend(config, { isClient }) {
+      if (isClient) config.devtool = '#source-map';
+    }
+  },
   buildModules: ['@nuxt/typescript-build'],
   css: ['@/assets/app'],
   loading: '~/components/molecules/MLoadingPanel/index.vue',
